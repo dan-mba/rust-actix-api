f8b9a321e21a8c1328334dcff2f1f280aaff33d1
Dan <35927536+dan-mba@users.noreply.github.com>
2023-05-10 14:22:14 -0400

    use env vars for ip & port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::{middleware, web, App, HttpResponse, HttpServer};
 use serde::Serialize;
 use log::info;
+use std::env;
+
 
 #[derive(Serialize)]
 struct MyResp {
@@ -16,9 +18,23 @@ async fn ping() -> HttpResponse {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let ip_address = match env::var("IP_ADDRESS") {
+        Ok(p) => p,
+        Err(_e) => String::from("127.0.0.1"),
+    };
+    
+    let port_str = match env::var("PORT") {
+        Ok(p) => p,
+        Err(_e) => String::from("8080"),
+    };
+    let port = match port_str.parse::<u16>() {
+        Ok(p) => p,
+        Err(_e) => 8080 as u16,
+    };
+
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    info!("starting HTTP server at http://localhost:8080");
+    info!("starting HTTP server at http://{ip_address}:{port}");
 
     HttpServer::new(|| {
         App::new()
@@ -27,7 +43,7 @@ async fn main() -> std::io::Result<()> {
             .service(web::resource("/ping").route(web::get().to(ping)))
     })
     .workers(1)
-    .bind(("127.0.0.1", 8080))?
+    .bind((ip_address, port))?
     .run()
     .await
 }
